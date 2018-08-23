199c543b801b7119864468e61cdb5a8c10a42994
Joe Timmins <joe@novoda.com>
2018-08-23 11:45:38 +0100

    All tests are passing

--- app/src/main/java/com/novoda/tennis/Tennis.kt
+++ app/src/main/java/com/novoda/tennis/Tennis.kt
@@ -2,9 +2,43 @@ package com.novoda.tennis
 
 class Game(private val playerOne: Player, private val playerTwo: Player) {
 
+    init {
+        playerOne.otherPlayerIsOnForty = { playerTwo.currentScore == Point.FORTY }
+        playerTwo.otherPlayerIsOnForty = { playerOne.currentScore == Point.FORTY }
+    }
+
     fun score(): String {
-        return ""
+        if (playerOne.currentScore == Point.GAME) return "${playerOne.name} wins!"
+        if (playerTwo.currentScore == Point.GAME) return "${playerTwo.name} wins!"
+
+        if (playerOne.currentScore == Point.FORTY && playerTwo.currentScore == Point.FORTY) return "Deuce"
+
+        if (playerOne.currentScore == Point.ADVANTAGE) return "${Point.ADVANTAGE.asString} ${playerOne.name}"
+        if (playerTwo.currentScore == Point.ADVANTAGE) return "${Point.ADVANTAGE.asString} ${playerTwo.name}"
+
+        return "${playerOne.currentScore.asString} - ${playerTwo.currentScore.asString}"
     }
 }
 
-data class Player(private val name: String)
+class Player constructor(val name: String) {
+    var currentScore: Point = Point.LOVE
+    lateinit var otherPlayerIsOnForty: () -> Boolean
+
+    fun scoresPoint() {
+        if (currentScore == Point.GAME) return
+        currentScore = (if (currentScore == Point.FORTY && otherPlayerIsOnForty()) Point.ADVANTAGE else nextPoint())
+    }
+
+    private fun nextPoint(): Point {
+        return if (currentScore == Point.FORTY) Point.GAME else Point.values()[currentScore.ordinal + 1]
+    }
+}
+
+enum class Point(val asString: String) {
+    LOVE("Love"),
+    FIFTEEN("Fifteen"),
+    THIRTY("Thirty"),
+    FORTY("Forty"),
+    ADVANTAGE("Advantage"),
+    GAME("Game")
+}
--- app/src/test/java/com/novoda/tennis/TennisTest.kt
+++ app/src/test/java/com/novoda/tennis/TennisTest.kt
@@ -5,14 +5,69 @@ import org.junit.Test
 
 class TennisTest {
 
-    private val roger = Player("Roger")
-    private val rafa = Player("Rafa")
+    private val roger = Player(name = "Roger")
+    private val rafa = Player(name = "Rafa")
     private val game = Game(roger, rafa)
 
     @Test
     fun `initial score should be love all`() {
-        val score = game.score()
+        assertThat(game.score()).isEqualTo("Love - Love")
+    }
 
-        assertThat(score).isEqualTo("Love - Love")
+    @Test
+    fun `score should be thirty all after both players score two points`() {
+        roger.scoresPoint()
+        roger.scoresPoint()
+        rafa.scoresPoint()
+        rafa.scoresPoint()
+
+        assertThat(game.score()).isEqualTo("Thirty - Thirty")
+    }
+
+    @Test
+    fun `score should be forty love after first player scores three points`() {
+        roger.scoresPoint()
+        roger.scoresPoint()
+        roger.scoresPoint()
+
+        assertThat(game.score()).isEqualTo("Forty - Love")
+    }
+
+    @Test
+    fun `score tells us which player has won the game`() {
+        roger.scoresPoint()
+        roger.scoresPoint()
+        roger.scoresPoint()
+        roger.scoresPoint()
+
+        assertThat(game.score()).isEqualTo("Roger wins!")
+    }
+
+    @Test
+    fun `score goes to deuce when both players score three points`() {
+        roger.scoresPoint()
+        roger.scoresPoint()
+        roger.scoresPoint()
+
+        rafa.scoresPoint()
+        rafa.scoresPoint()
+        rafa.scoresPoint()
+
+        assertThat(game.score()).isEqualTo("Deuce")
+    }
+
+    @Test
+    fun `when score is deuce, next player to score a point goes to advantage`() {
+        roger.scoresPoint()
+        roger.scoresPoint()
+        roger.scoresPoint()
+
+        rafa.scoresPoint()
+        rafa.scoresPoint()
+        rafa.scoresPoint()
+
+        roger.scoresPoint()
+
+        assertThat(game.score()).isEqualTo("Advantage Roger")
     }
 }
